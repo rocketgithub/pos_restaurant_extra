32997e09d24ce3e3cbcca6629ab327490d0794f7
Rodrigo Fernandez <rodrigo@solucionesprisma.com>
2020-10-22 18:07:49 +0000

    Errores varios

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import pos_config
-from . import pos_extra
 from . import pos_order
-from . import res_users
--- static/src/js/pos_gt.js
+++
@@ -1,4 +0,0 @@
-odoo.define('pos_restaurant_extra.pos_restaurant_extra', function (require) {
-"use strict";
-
-});
